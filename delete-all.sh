3422cd054c1df4ace067cbcd3af66b6751a92a07
Amitoj Hanspal <amitojsh@hotmail.com>
2020-06-06 18:18:10 +0100

    microk8s changes

---
+++ delete-all.sh
@@ -0,0 +1,3 @@
+kubectl delete -f ingress.yml
+kubectl delete -f foo-deployment.yml
+kubectl delete -f bar-deployment.yml
--- docker-build-tag-push.sh
+++ docker-build-tag-push.sh
@@ -1,3 +1,3 @@
-docker build . -t my-microk8s-app
-docker tag my-microk8s-app localhost:32000/my-microk8s-app
-docker push localhost:32000/my-microk8s-app
+sudo docker build . -t my-microk8s-app
+sudo docker tag my-microk8s-app localhost:32000/my-microk8s-app
+sudo docker push localhost:32000/my-microk8s-app
